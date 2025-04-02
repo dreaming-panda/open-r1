ecc31682d620b26434759d754825fa1ef85b99f3
dreaming-panda <chenzhuoming911@gmail.com>
2025-04-02 14:33:20 -0400

    P25

--- train_7B_sparse.sh
+++ train_7B_sparse.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft_relu.py  --config recipes/OpenR1-Qwen-7B/sft/config7B_sparse3.yaml
+accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft_relu.py  --config recipes/OpenR1-Qwen-7B/sft/config7B_P25.yaml
