3a454e000e4533fd7c5cfc9de369aa1ec291e2de
dreaming-panda <chenzhuoming911@gmail.com>
2025-04-05 13:44:30 -0400

    adjlr

--- train_7B_L32.sh
+++ train_7B_L32.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft_conv.py  --config recipes/OpenR1-Qwen-7B/sft/config7BL32.yaml
+accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft_adj_lr.py  --config recipes/OpenR1-Qwen-7B/sft/config7BL32.yaml
