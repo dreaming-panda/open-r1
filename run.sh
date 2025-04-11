05d0754976d9838da227c7b85e21eb892072d95f
dreaming-panda <64370186+dreaming-panda@users.noreply.github.com>
2025-04-10 21:06:49 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+
+git remote set-url origin https://dreaming-panda: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@github.com/dreaming-panda/open-r1.git
+git pull
+bash run_job.sh
+git commit -m "run"
+git push 
+
