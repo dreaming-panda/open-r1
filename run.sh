e43c823878dc864bfee9376069fd9878db2d4278
dreaming-panda <64370186+dreaming-panda@users.noreply.github.com>
2025-04-10 21:08:29 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 git remote set-url origin https://dreaming-panda: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@github.com/dreaming-panda/open-r1.git
 git pull
 bash run_job.sh
+git add *
 git commit -m "run"
 git push 
 
