d3b1d60cab7f02fc8cf6d1b354adf2c2c48239a5
dreaming-panda <chenzhuoming911@gmail.com>
2025-04-12 15:09:12 -0400

    sft

--- train_1.5B_L28.sh
+++ train_1.5B_L28.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=3,5,6,7
-accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft_relu.py  --config recipes/OpenR1-Qwen-7B/sft/config1.5B_sparse.yaml
+accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft.py  --config recipes/OpenR1-Qwen-7B/sft/config1.5B.yaml
