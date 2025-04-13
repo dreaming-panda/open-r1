7d7e73f9ba6ed331dae024ec2fb20d40c014e208
dreaming-panda <chenzhuoming911@gmail.com>
2025-04-12 22:50:41 -0400

    recover

--- train_7B_EP.sh
+++ train_7B_EP.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 export WANDB_PROJECT="sparsity"
-python src/open_r1/fat_save.py --multiplier 3
+python src/open_r1/fat_save.py --multiplier 2
+huggingface-cli download ZMC2019/Qwen7B-EP --local-dir data/Qwen7B-EP
 accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft.py  --config recipes/OpenR1-Qwen-7B/sft/config7B.yaml
