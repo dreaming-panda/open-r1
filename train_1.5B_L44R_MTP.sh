99bd53712495c635d9c8dca54fad427a17ce3f15
dreaming-panda <chenzhuoming911@gmail.com>
2025-04-13 20:18:39 -0400

    mtp

--- train_1.5B_L44R_MTP.sh
+++ train_1.5B_L44R_MTP.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+export WANDB_PROJECT="MTP"
 python src/open_r1/mtp_init.py 
 accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft_mtp.py  --config recipes/OpenR1-Qwen-7B/sft/config1.5BMTP.yaml
