d1fd1801cd24992f47f574b8812593629dde25bc
dreaming-panda <chenzhuoming911@gmail.com>
2025-04-15 01:41:35 -0400

    lora

--- train_1.5B_MTPLORA.sh
+++ train_1.5B_MTPLORA.sh
@@ -1,4 +1,5 @@
 
+export WANDB_PROJECT="MTP"
 python src/open_r1/mtp3_save.py
 accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft_mtp3.py  --config recipes/OpenR1-Qwen-7B/sft/config1.5BMTPLORA.yaml
 
