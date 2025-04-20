594a00ee1ac23b29752fb0c4db5de397be47aaba
dreaming-panda <chenzhuoming911@gmail.com>
2025-04-19 21:54:51 -0400

    L44

--- train_1.5B_L44sto.sh
+++ train_1.5B_L44sto.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export WANDB_PROJECT="Stochastic"
 python src/open_r1/unroll.py --start 12 --end 16 --num_conv 4 --output_dir Qwen1.5B-L44-Flat
+huggingface-cli download ZMC2019/Qwen1.5B-L44-Stochastic-tuned --local-dir data/Qwen1.5B-L44-Stochastic-tuned
 accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft_stochas.py  --config recipes/OpenR1-Qwen-7B/sft/config1.5BL44.yaml
