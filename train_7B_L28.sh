4b2c7bda9e0da4dd8f3ea3b5df0875d8a6bdec62
dreaming-panda <chenzhuoming911@gmail.com>
2025-04-20 23:41:20 -0400

    math

--- train_7B_L28.sh
+++ train_7B_L28.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export WANDB_PROJECT="MATH"
-huggingface-cli download ZMC2019/Qwen7B-Baseline --local-dir data/Qwen7B-Baseline
-accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft.py  --config recipes/OpenR1-Qwen-7B/sft/config7B.yaml
+huggingface-cli ZMC2019/Qwen7B-Math-L28 --local-dir data/Qwen7B-Math-L28
+accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft.py  --config recipes/OpenR1-Qwen-7B/sft/config7BMath.yaml
