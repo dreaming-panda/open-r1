d1939b606eb5c5b1567b74ae1cdd64e98fe9b21c
dreaming-panda <chenzhuoming911@gmail.com>
2025-04-21 00:35:57 -0400

    Math

--- train_7B_L32.sh
+++ train_7B_L32.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export WANDB_PROJECT="MATH"
 python src/open_r1/unroll.py --start 12 --end 16 --num_conv 1 --model ZMC2019/Qwen2.5-Math-7B-Instruct --output_dir Qwen7B-Math-L32
-huggingface-cli ZMC2019/Qwen7B-Math-L32 --local-dir data/Qwen7B-Math-L32
+huggingface-cli download ZMC2019/Qwen7B-Math-L32 --local-dir data/Qwen7B-Math-L32
 accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft.py  --config recipes/OpenR1-Qwen-7B/sft/config7BL32.yaml
