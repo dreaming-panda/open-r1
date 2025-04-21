30873e0061319334c337354f747dbe5f1c4b078d
dreaming-panda <chenzhuoming911@gmail.com>
2025-04-21 13:34:02 -0400

    rl

--- rl_1.5B_L28.sh
+++ rl_1.5B_L28.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-ACCELERATE_LOG_LEVEL=info accelerate launch --config_file recipes/accelerate_configs/zero2.yaml \
-    --num_processes=7 src/open_r1/grpo.py \
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+trl vllm-serve --model ZMC2019/Qwen1.5B-L28-90K --tensor_parallel_size 4 --gpu_memory_utilization 0.9 &
+
+sleep 120
+
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+accelerate launch --config_file recipes/accelerate_configs/zero2.yaml \
+    --num_processes=4 src/open_r1/grpo.py \
     --config recipes/Qwen2.5-1.5B-Instruct/grpo/configL28.yaml
