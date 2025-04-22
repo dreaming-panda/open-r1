0ecb734a415fa5bb83df42e597499f60ba3ccd5d
dreaming-panda <chenzhuoming911@gmail.com>
2025-04-22 15:01:40 -0400

    MathMPHP

---
+++ train_7B_MathMP.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export WANDB_PROJECT="MATH"
+python src/open_r1/make_mlp.py --scaler 2 --model ZMC2019/Qwen2.5-Math-7B-Instruct --output_dir Qwen7B-Math-L28-MP
+accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft.py  --config recipes/OpenR1-Qwen-7B/sft/config7BMathMP.yaml
